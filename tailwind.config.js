07378f5b4ec75980cccb0a31f75887ce4a2fc742
Jason Blackert <jason.blackert@gmail.com>
2026-05-05 21:15:15 -0400

    (WIP)  of new layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,13 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      height: {
+        '84': '21rem',
+      },
       boxShadow: {
         neon: "0 0 5px theme('colors.indigo.200'), 0 0 20px theme('colors.indigo.700')",
         low: "0 0 5px theme('colors.indigo.400')",
+        dim: "0 0 8px theme('colors.indigo.900')",
       }
     },
   },
